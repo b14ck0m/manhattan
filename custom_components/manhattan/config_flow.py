92906df0f60aec9f9623fc087c009a94e9098658
blackom <local@local>
2023-04-23 14:14:23 -0400

    change step to deviceID from init

--- custom_components/manhattan/config_flow.py
+++ custom_components/manhattan/config_flow.py
@@ -74,7 +74,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         _LOGGER.info("user step")
 
         if user_input is None:
-            return self.async_show_form(step_id="init",
+            return self.async_show_form(step_id="deviceID",
                                         data_schema=vol.Schema(data_schema_user),
                                         errors=self._errors)
         
