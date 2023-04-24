c695e3daf029e1f1aedc2abebc8117608a3bccde
blackom <local@local>
2023-04-24 10:13:28 -0400

    fix counting variable

--- custom_components/manhattan/config_flow.py
+++ custom_components/manhattan/config_flow.py
@@ -141,11 +141,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                                         errors=self._errors)
         _LOGGER.info(pformat(user_input))
         self.data[CONF_NAME].append(self.data[CONF_NAME]);
-        self.data[CONF_PATH].append(self.data["counting"]);
+        self.data[CONF_PATH].append(self.counting);
         self.counting +=1;
         if self.counting < self.data[RELAY_COUNT]:
             return self.async_show_form(step_id="relay_name",
                                         data_schema=vol.Schema(data_schema_relay),
-                                        description_placeholders={"name":str(self.counting)},
+                                        description_placeholders={"name":self.counting},
                                         errors=self._errors)
         return self.async_create_entry(title=DOMAIN,data=self.data)
